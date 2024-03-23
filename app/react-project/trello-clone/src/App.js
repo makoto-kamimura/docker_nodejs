e2f663b3853ac2a4ad9234aa83d565c12d02313f
Makoto Kamimura <68632305+makoto-kamimura@users.noreply.github.com>
2024-03-23 16:39:48 +0900

    update react-project

--- app/react-project/trello-clone/src/App.js
+++ app/react-project/trello-clone/src/App.js
@@ -1,10 +1,66 @@
-import logo from './logo.svg';
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState(["item0", "item1", "item2"]);
+  const onDragEnd = (result) => {
+    // 削除
+    const remove = items.splice(result.source.index, 1);
+    // 挿入
+    items.splice(result.destination.index, 0, remove[0]);
+    // ステートを更新
+    setItems([...items]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
+    // <div className="App">
+    <div className="dragDropArea">
+<DragDropContext onDragEnd={onDragEnd}>
+  <Droppable droppableId="droppable">
+    {(provided) => (
+      <div ref={provided.innerRef} {...provided.droppableProps}>
+        <Draggable draggableId="item0" index={0}>
+          {(provided) => (
+            <div
+              className="item"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              {items[0]}
+            </div>
+          )}
+        </Draggable>
+        <Draggable draggableId="item1" index={1}>
+          {(provided) => (
+            <div
+              className="item"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              {items[1]}
+            </div>
+          )}
+        </Draggable>
+        <Draggable draggableId="item2" index={2}>
+          {(provided) => (
+            <div
+              className="item"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              {items[2]}
+            </div>
+          )}
+        </Draggable>
+        {provided.placeholder}
+      </div>
+    )}
+  </Droppable>
+</DragDropContext>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +73,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ npx 10.2.4
 
 ```
 <!-- docker build . -t docker_nodejs -->
-docker-compose up -d 
+docker-compose up -d
 docker-compose down
 ```
 
@@ -16,5 +16,18 @@ mkdir react-project
 cd react-project
 npx create-react-app trello-clone
 cd trello-clone
+yarn add react-beautiful-dnd
 yarn start
 ```
+
+```
+docker exec -it docker_nodejs-app-1 sh
+cd react-project/trello-clone
+yarn start
+```
+http://localhost:3000/
+
+##
+yarn add react@18.2.0 react-dom@18.2.0
+// <React.StrictMode>
+yarn add --dev @babel/plugin-proposal-private-property-in-object
